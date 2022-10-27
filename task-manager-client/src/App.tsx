b754e9937ad895e873493e5f2c6d2344847539ff
hamzaabde <xamsze1234@gmail.com>
2022-10-27 11:02:08 +0300

    Initial commit

---
+++ task-manager-client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+import MainLayout from "./layout/MainLayout";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ task-manager-client/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.tsx", "src/**/.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+};
