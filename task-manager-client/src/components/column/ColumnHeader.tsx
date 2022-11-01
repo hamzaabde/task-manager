7be8c673dc94da63792d38b3d9f07e15a4295249
hamzaabde <xamsze1234@gmail.com>
2022-11-01 22:29:15 +0300

    Made a quick and temporary fix to ColumnHeader colors

--- task-manager-client/src/components/column/ColumnHeader.tsx
+++ task-manager-client/src/components/column/ColumnHeader.tsx
@@ -1,23 +1,34 @@
 import type { ReactElement } from "react";
 
+import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
+import { TaskColumnColors } from "../../constants/ui";
+
 type props = {
   title: string;
   color?: string;
   tasksCount: number;
 };
 
-import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
+const colors = {
+  RED_className: "bg-red-500",
+  BLUE_className: "bg-blue-500",
+  GREEN_className: "bg-green-500",
+  PURPLE_className: "bg-purple-500",
+  YELLOW_className: "bg-yellow-500",
+};
 
 export default function ColumnHeader({
   title,
   color,
   tasksCount,
 }: props): ReactElement {
+  const colorArray = Object.entries(colors);
+  const randomColor =
+    colorArray[Math.floor(Math.random() * colorArray.length)][1];
+
   return (
     <div className='mb-4 flex cursor-default items-center gap-2'>
-      <div
-        className={`h-5 w-5 rounded-full ${color ? color : "bg-purple-500"}`}
-      ></div>
+      <div className={`h-5 w-5 rounded-full ${randomColor}`}></div>
       <p className='justify-self-start font-semibold capitalize text-base-content/80'>
         {title} ({tasksCount})
       </p>
